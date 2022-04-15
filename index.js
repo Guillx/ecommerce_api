00c67491c6cbd24efc6fa294cdc1d43c09d949a1
Guillx <guillx.gv@gmail.com>
2022-04-15 03:09:13 +0200

    Created models and routes templates and added firts route (auth)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -12,9 +14,9 @@ mongoose
     console.log(err);
   });
 
-app.get("/api/test", () => {
-  console.log("Test correcto");
-});
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5050, () => {
   console.log("Servidor Backend funcionando");
