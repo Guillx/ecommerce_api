b0d4c16464085712c69db8c554d2d021dc0e4fe5
Guillx <guillx.gv@gmail.com>
2022-04-15 16:18:45 +0200

    Created CRUD product routes

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+const productRoute = require("./routes/product");
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 app.listen(process.env.PORT || 5050, () => {
   console.log("Servidor Backend funcionando");
